ebff507ed99144f9e186f36b69c6a9c785067faa
Hitsam Tiammar <hitsamtiammm@gmail.com>
2019-08-19 16:19:15 +0700

    penambahan lang id dan perubahan settingan default lang dari en ke id

---
+++ resources/lang/id/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'ID pengguna atau password yang anda masukan salah',
+    'throttle' => 'Terlalu banyak login, silakan coba kembali dalam :seconds detik.',
+
+];
