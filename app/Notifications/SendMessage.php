81a8e1f5dc956ede90473a3d2d474b9386951392
Hitsam Tiammar <hitsamtiammm@gmail.com>
2019-08-28 00:05:06 +0700

    hapus2 komen gak penting

--- app/Notifications/SendMessage.php
+++ app/Notifications/SendMessage.php
@@ -20,34 +20,19 @@ class SendMessage extends Notification
     }
 
 
-    /**
-     * Get the notification's delivery channels.
-     *
-     * @param  mixed  $notifiable
-     * @return array
-     */
+
     public function via($notifiable)
     {
         return ['database'];
     }
 
-    /**
-     * Get the mail representation of the notification.
-     *
-     * @param  mixed  $notifiable
-     * @return \Illuminate\Notifications\Messages\MailMessage
-     */
+
     public function toMail($notifiable)
     {
 
     }
 
-    /**
-     * Get the array representation of the notification.
-     *
-     * @param  mixed  $notifiable
-     * @return array
-     */
+
     public function toArray($notifiable)
     {
         return [
