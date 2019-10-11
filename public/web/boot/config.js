4fd88daa593a8e2367fe99ae9afa7db94db011d3
Hitsam Tiammar <hitsamtiammm@gmail.com>
2019-10-12 00:58:23 +0700

    bug fixing untuk tampilan table di firefox dan edge

--- public/web/boot/config.js
+++ public/web/boot/config.js
@@ -1,5 +1,13 @@
 app.config(function(cPProvider,$routeProvider,$locationProvider,routingProvider,$rootScopeProvider,formModalProvider){
-    var argList=arguments.callee.toString().match(/function\((.+)\)/)[1].split(',');
+
+    //var argList=arguments.callee.toString().match(/function\((.+)\)/)[1].split(',');
+    var argList=[
+        'cPProvider',
+        '$routeProvider',
+        '$locationProvider',
+        'routingProvider',
+        '$rootScopeProvider'
+    ];
     for(var i=0;i<argList.length;i++){
         var a=argList[i];
         var arg=arguments[i];
