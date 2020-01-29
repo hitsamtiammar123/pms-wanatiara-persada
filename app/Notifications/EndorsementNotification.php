d5f7a50f4fca0a1628a1624dbbc5a84a945db778
hitsamtiammar123 <hitsamtiammmar@gmail.com>
2020-01-29 14:22:15 +0700

    update

--- app/Notifications/EndorsementNotification.php
+++ app/Notifications/EndorsementNotification.php
@@ -21,10 +21,13 @@ class EndorsementNotification extends Notification
         $link='';
         if($this->header instanceof \App\Model\KPIHeader){
             $employee_id=$this->header->employee->id;
-            $link="realisasi/$employee_id/".($this->header->cPeriod()->month-1);
+            $cdate=$this->header->cPeriod();
+            $link="realisasi/$employee_id/".($cdate->month-1).'/'.$cdate->year;
         }
         else if($this->header instanceof \App\Model\KPITag){
-            $link="realisasi-group/{$this->header->id}";
+            $e_zero=$this->header->getZeroIndexEmployee();
+            $cdate=$e_zero?$e_zero->getCurrentHeader()->cPeriod():Carbon::now();
+            $link="realisasi-group/{$this->header->id}".($cdate->month-1).'/'.$cdate->year;
         }
 
         return $link;
@@ -33,7 +36,7 @@ class EndorsementNotification extends Notification
     protected function getMessage(){
         $result='';
         $header=$this->header;
-       
+
         if($header instanceof \App\Model\KPIHeader){
             $message="%s sudah mengesahkan %s untuk periode %s";
             $employee_name=$this->employee->name;
@@ -51,7 +54,7 @@ class EndorsementNotification extends Notification
             $result=sprintf($message,$employee_name,$headerTo,$period);
         }
         else if($header instanceof \App\Model\KPITag){
-           
+
             $message="%s sudah mengesahkan %s";
             $employee_name=$this->employee->name;
             $headerTo="PMS group dari \"{$header->name}\"";
